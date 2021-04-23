51b3d51275c6f9b444270a2077ef6ad4c479b203
damilare <damilare.durojaye@gmail.com>
2021-04-23 20:09:59 +0200

    Vuetify component: Used snackbar and datatable to display a row and list of sortable data.

--- src/App.vue
+++ src/App.vue
@@ -52,6 +52,10 @@ export default {
           {
               label: 'Login',
               url: '/login'
+          },
+          {
+              label: 'Dashboard',
+              url: '/dashboard'
           }
       ]
   }),
