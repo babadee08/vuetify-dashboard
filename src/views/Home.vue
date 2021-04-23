f5f9fb104982ea8f6eac28c8421b935fa2fb300a
damilare <damilare.durojaye@gmail.com>
2021-04-23 19:50:58 +0200

    Vue router: Added the router and extracted home and login component

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld";
+
+export default {
+    name: "Login",
+
+    components: {
+        // eslint-disable-next-line vue/no-unused-components
+        HelloWorld,
+    }
+}
+</script>
